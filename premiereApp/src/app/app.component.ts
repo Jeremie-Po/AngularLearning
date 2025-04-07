de32b147b48762e96aaa1189a64d8d5959c37ad9
Jérémie P <jpourageaux@hotmail.fr>
2025-04-07 16:56:21 +0200

    creat basique componant - sfc componant - configuration for sfc cli

--- premiereApp/src/app/app.component.ts
+++ premiereApp/src/app/app.component.ts
@@ -1,12 +1,14 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component} from '@angular/core';
+import {RouterOutlet} from '@angular/router';
+import {DymaComponent} from './dyma/dyma.component';
+import {DymaSFCComponent} from './dyma-sfc.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, DymaComponent, DymaSFCComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'premiereApp';
+  title = 'proot';
 }
---
+++ premiereApp/src/app/dyma-sfc.component.ts
@@ -0,0 +1,19 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'app-dyma-sfc',
+  imports: [],
+  template: `
+    <p class="red">
+      dyma-sfc works!
+    </p>
+  `,
+  styles: `
+    .red {
+      color: red;
+    }
+  `
+})
+export class DymaSFCComponent {
+
+}
