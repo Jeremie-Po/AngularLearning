76e56e9705e6c0eb022fd4e619267cd06a03a75d
Jérémie P <jpourageaux@hotmail.fr>
2025-04-16 12:58:03 +0200

    chnager la langue local pour l appli de us en fr

--- premiereApp/src/app/app.config.ts
+++ premiereApp/src/app/app.config.ts
@@ -1,8 +1,21 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import {ApplicationConfig, LOCALE_ID, provideZoneChangeDetection} from '@angular/core';
+import {provideRouter} from '@angular/router';
 
-import { routes } from './app.routes';
+import {routes} from './app.routes';
+
+import fr from '@angular/common/locales/fr';
+import {registerLocaleData} from '@angular/common';
+
+registerLocaleData(fr);
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({eventCoalescing: true}),
+    provideRouter(routes),
+    {
+      provide: LOCALE_ID,
+      useValue: 'fr-FR',
+    },
+
+  ]
 };
