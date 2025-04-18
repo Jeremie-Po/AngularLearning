fee525ee5d0a4fab00f474f30a55ad44057f7b08
Jérémie P <jpourageaux@hotmail.fr>
2025-04-18 14:18:44 +0200

    mise en place des premieres routes et leur affichage grace  router-outlet

--- premiereApp/src/app/app.routes.ts
+++ premiereApp/src/app/app.routes.ts
@@ -1,3 +1,14 @@
-import { Routes } from '@angular/router';
+import {Routes} from '@angular/router';
+import {HomePageComponent} from './components/home-page.component';
+import {UsersComponent} from './components/users.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: HomePageComponent,
+  },
+  {
+    path: 'users',
+    component: UsersComponent,
+  }
+];
