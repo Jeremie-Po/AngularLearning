d157296c3072d50ac1818354a6a768e0ff2ce762
Jérémie P <jpourageaux@hotmail.fr>
2025-04-18 14:48:55 +0200

    exemple redirection et wil card

--- premiereApp/src/app/app.routes.ts
+++ premiereApp/src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import {Routes} from '@angular/router';
 import {HomePageComponent} from './components/home-page.component';
 import {UsersComponent} from './components/users.component';
+import {NotFoundComponent} from './components/not-found.component';
 
 export const routes: Routes = [
   {
@@ -12,8 +13,22 @@ export const routes: Routes = [
     //pathmatch full permet de faire matcher la route exact, prefix 'doit commencer par'
     pathMatch: 'full'
   },
+
+
+  {
+    path: 'toto',
+    // redirect permet de rediriger l'url vers une autre'
+    redirectTo: 'users',
+  },
+
   {
     path: '',
     component: HomePageComponent,
   },
+
+  {
+    // route wild Cart qui permet d'être utiliser qd aucune route n'existe
+    path: '**',
+    component: NotFoundComponent,
+  }
 ];
