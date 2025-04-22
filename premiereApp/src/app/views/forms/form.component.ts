c3cf915b3873875ce0c71a87f0773bba54e3dfd6
Jérémie P <jpourageaux@hotmail.fr>
2025-04-22 12:05:21 +0200

    create html form

--- premiereApp/src/app/views/forms/form.component.ts
+++ premiereApp/src/app/views/forms/form.component.ts
@@ -1,14 +1,41 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-form',
   imports: [],
   template: `
-    <p>
-      form works!
-    </p>
+    <form class="w-half">
+      <div class="flex flex-col mb-10">
+        <label for="lastName">Nom</label>
+        <input type="text" id="lastName">
+      </div>
+      <div class="flex flex-col mb-10">
+        <label for="firstname">Prénom</label>
+        <input type="text" id="firstname">
+      </div>
+      <div class="flex flex-col mb-10">
+        <label for="email">Email</label>
+        <input type="text" id="email">
+      </div>
+      <div class="flex flex-col mb-20">
+        <label for="password">Mot de passe</label>
+        <input type="password" id="password">
+      </div>
+      <button class="btn btn-primary">Sauvegarder</button>
+
+    </form>
+    <div class="w-half"></div>
   `,
-  styles: ``
+  styles: `
+    :host {
+      display: flex;
+      width: 100%;
+      max-width: 900px;
+      margin: auto;
+      padding: 24px;
+      gap: 25px;
+      /*border: 1px solid green;*/
+    }`
 })
 export class FormComponent {
 
